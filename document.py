4d25638d0134c787d2989565b786900db5b68cd7
EzhoffDmitriy <ezhoff.dmitriy@gmail.com>
2024-12-12 13:35:11 +0300

    add json

--- document.py
+++ document.py
@@ -1,29 +1,45 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 import json
 import types as tp
 
+
 @dataclass(order=False, eq=False, repr=False)
 class Document:
-    '''
+    """
     class Document
-    все модули меняют меняют поле text, 
-    preprocessed_text нужен для классификатора (нужно хранить и то и то) (mb тоже в метадата ?), 
-    в metadata будем хранить некотые теги для микросервисов (например тег : договор). 
-    TODO: json
-    '''
-    text: str
-    preprocessed_text: str = ''
+    все модули меняют меняют поле text,
+    preprocessed_text нужен для классификатора (нужно хранить и то и то) (mb тоже в метадата ?),
+    в metadata будем хранить некотые теги для микросервисов (например тег : договор).
+    """
 
-    metadata : dict[str, str | int | list[str]] = field(default_factory=dict)
+    text: str
+    name: str = ""
+    preprocessed_text: str = ""
+
+    metadata: dict[str, str | int | list[str]] = field(default_factory=dict)
 
     def __eq__(self, other):
         return self.text == other.text
+
     def __repr__(self):
         return self.text
-    
+
     @classmethod
     def parse_json(cls, file):
-        pass
+        # TODO : переделать
+        with open(file, "r") as f:
+            data = json.load(f)
+        res = Document(
+            text=data["text"],
+            name=data["name"],
+            preprocessed_text=data["preprocessed_text"],
+            metadata=data["metadata"],
+        )
+        return res
+
     @classmethod
-    def to_json(cls, file):
-        pass
+    def to_json(cls, doc, file):
+        if doc.name == "":
+            doc.name = file[: file.find(".")]
+        with open(file, "w") as f:
+            json.dump(asdict(doc), f)
