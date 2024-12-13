4bd390589bfcf405566f6906d33558fdf80a63ad
EzhoffDmitriy <ezhoff.dmitriy@gmail.com>
2024-12-13 19:39:48 +0300

    add todo

--- summarizer.py
+++ summarizer.py
@@ -2,24 +2,26 @@ from document import Document
 import types as tp
 import transformers
 
+# TODO: test end chose best model!!!
 MODELS = ["d0rj/rut5-base-summ",]
+
 
 class Summarizer:
     """
     summarize main data from document, using small llm
     """
-    def __init__(self, model:str, tokenizer: str | None = None):
+
+    def __init__(self, model: str, tokenizer: str | None = None):
         self.pipe = transformers.pipeline('summarization', model=model)
 
-    def sum_list(self, data:list[Document], save_sum = False)->list[str]:
+    def sum_list(self, data: list[Document], save_sum=False) -> list[str]:
         res = []
         for i in data:
             res.append(self.sum(i, save_sum))
         return res
 
-    def sum(self, doc:Document, save_sum = False) -> str:
+    def sum(self, doc: Document, save_sum=False) -> str:
         s = self.pipe(doc.text)
         if save_sum:
             i.sum = s
         return s
- 
