db492530ab8c1e242ea958c30f6743af04839a6c
EzhoffDmitriy <ezhoff.dmitriy@gmail.com>
2024-12-13 04:20:33 +0300

    123

--- summarizer.py
+++ summarizer.py
@@ -1,25 +1,7 @@
 from document import Document
+import tf_keras
 
 import transformers
-from transformers import pipeline
-import tensorflow as tf
-
-
-pipe = pipeline("summarization", model="d0rj/rut5-base-summ")
-pipe("lalalalala")
-
-model_id = "unsloth/llama-3-8b-Instruct-bnb-4bit"
-
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={
-        "torch_dtype": torch.float16,
-        "quantization_config": {"load_in_4bit": True},
-        "low_cpu_mem_usage": True,
-    },
-)
-
 
 class Summarizer:
     """
