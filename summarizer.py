09222867b37901e9a6949bc78b3781e7bfdb58c8
EzhoffDmitriy <ezhoff.dmitriy@gmail.com>
2024-12-13 04:08:39 +0300

    write main funcions у меня нихрена не робит трансформерс (

--- summarizer.py
+++ summarizer.py
@@ -1,7 +1,39 @@
 from document import Document
 
+import transformers
+from transformers import pipeline
+import tensorflow as tf
+
+
+pipe = pipeline("summarization", model="d0rj/rut5-base-summ")
+pipe("lalalalala")
+
+model_id = "unsloth/llama-3-8b-Instruct-bnb-4bit"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={
+        "torch_dtype": torch.float16,
+        "quantization_config": {"load_in_4bit": True},
+        "low_cpu_mem_usage": True,
+    },
+)
+
 
 class Summarizer:
     """
     summarize main data from document, using small llm
     """
+    def __init__(self, model:str, tokenizer: str | None):
+        self.pipe = transformers.pipeline('summarization', model=model)
+
+    def sum(self, data=tp.List[Document], save_sum = False) -> tp.List[str]:
+        res = []
+        for i in data:
+            s = self.pipe(i.text)
+            if save_sum:
+                i.sum = s
+            res.append(s)
+        return res
+
